b9ac5f90288872606a8a413d1e401a97a4591b02
YawningLiu <772777264@qq.com>
2019-09-03 12:15:43 +0800

    hw

---
+++ ex3/predict.m
@@ -0,0 +1,36 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+
+% 模拟实现前向传播算法
+X = [ones(m, 1) X];
+h1 = sigmoid(Theta1 * X');
+h1 = [ones(1,m); h1];% add bias unit
+anss = sigmoid(Theta2 * h1);
+%==================================
+[~,p] = max( anss',[],2);
+% 对样本的结果进行预测，与逻辑回归的预测类似，选取输出的最大值 作为最终的预测结果
+
+% =========================================================================
+
+
+end
